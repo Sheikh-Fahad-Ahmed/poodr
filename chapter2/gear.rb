7ea2e9cfbc44ffa6efec7e5db5984a4c31d1dcb7
Fahad Ahmed <fahad838@gmail.com>
2024-09-26 18:38:06 +0530

    Refactor wheel logic for better readability

--- chapter2/gear.rb
+++ chapter2/gear.rb
@@ -1,10 +1,12 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  private
+  attr_reader :chainring, :cog, :wheel
+
+  public
   def initialize(chainring,cog,rim,tire)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
+    @wheel = Wheel.new(rim,tire)
   end
 
   def ratio
@@ -12,9 +14,14 @@ class Gear
   end
 
   def gear_inches
-    ratio * (rim + (tire * 2))
+    ratio * wheel.diameter
+  end
+
+  Wheel = Struct.new(:rim,:tire) do
+    def diameter
+      rim + (tire * 2)
+    end
   end
 end
 
 
-puts Gear.new(52, 11, 24, 1.25).gear_inches
